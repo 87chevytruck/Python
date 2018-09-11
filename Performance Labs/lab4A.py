37b8b2d1bf80ccf84458ea692aff2f93f6261ff1
Ricky Smith <ricky.smith.8@outlook.com>
2018-09-11 09:54:35 -0500

    added 'screen' display, changed fib to itterative for faster result

--- Performance Labs/lab4A.py
+++ Performance Labs/lab4A.py
@@ -17,15 +17,15 @@ def power(x, y):
     return x ** y
 
 def fib(n):
-    if n <= -1:
-        return 0
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        # recursively calls fib function within the function
-        return fib(n-2) + fib(n-1)
+    a = 0
+    b = 1
+    c = 0
+
+    for i in range(n):
+        if c < 100:
+            a, b = b, a + b
+            c += 1
+    return a
 
 def fac(userNum):
     staticNum = userNum
@@ -42,6 +42,7 @@ def fac(userNum):
 
 input = "yes"
 while input == "yes":
+    print("Select a calculation option from the following options:")
     userInput = raw_input("1)  add two numbers\n2)  subtract two numbers\n3)  multiply two numbers\n4)  divide two numbers\n5)  power of two numbers\n6)  Factorial a number\n7)  fibonacci a number\n")
     userInput = int(userInput)
     if userInput == range(1,7):
@@ -54,8 +55,19 @@ while input == "yes":
             y = raw_input("What is the secound number you would like to add to the previous number?  ")
             y = float(y)
             z = lambda x, y: x+y
-            print("{} + {} = ").format(x, y),
-            print z(x, y)
+            # print("{} + {} = ").format(x, y),
+            # print z(x, y)
+            print("******************************")
+            print("*                            *")
+            print("*                            *")
+            print("*                            *")
+            print("*{:8.2f}     + {:8.2f} =   *").format(x, y)
+            print("*                            *")
+            print("*                            *")
+            print("*         {:8.2f}           *").format((z(x, y)))
+            print("*                            *")
+            print("*                            *")
+            print("******************************")
 
         elif userInput == 2:
             x = raw_input("What is the first number you would like to subtract?  ")
@@ -64,7 +76,18 @@ while input == "yes":
             y = float(y)
             z = lambda x, y: x-y
             print("{} - {} = ").format(x, y),
-            print z(x, y)
+            # print z(x, y)
+            print("******************************")
+            print("*                            *")
+            print("*                            *")
+            print("*                            *")
+            print("*{:8.2f}     - {:8.2f} =   *").format(x, y)
+            print("*                            *")
+            print("*                            *")
+            print("*         {:8.2f}           *").format((z(x, y)))
+            print("*                            *")
+            print("*                            *")
+            print("******************************")
 
         elif userInput == 3:
             x = raw_input("What is the first number you would like to multiply?  ")
@@ -72,7 +95,18 @@ while input == "yes":
             y = raw_input("What is the secound number you would like to multiply to the previous number?  ")
             y = float(y)
             z = multi(x, y)
-            print("{} x {} = {}").format(x, y, z)
+            # print("{} x {} = {}").format(x, y, z)
+            print("******************************")
+            print("*                            *")
+            print("*                            *")
+            print("*                            *")
+            print("*{:8.2f}     x {:8.2f} =   *").format(x, y)
+            print("*                            *")
+            print("*                            *")
+            print("*         {:8.2f}           *").format(z)
+            print("*                            *")
+            print("*                            *")
+            print("******************************")
 
         elif userInput == 4:
             x = raw_input("What is the first number you would like to divide?  ")
@@ -80,7 +114,18 @@ while input == "yes":
             y = raw_input("What is the secound number you would like to divide into the previous number?  ")
             y = float(y)
             z = divide(x, y)
-            print("{} / {} = {}").format(x, y, z)
+            # print("{} / {} = {}").format(x, y, z)
+            print("******************************")
+            print("*                            *")
+            print("*                            *")
+            print("*                            *")
+            print("*{:8.2f}     / {:8.2f} =   *").format(x, y)
+            print("*                            *")
+            print("*                            *")
+            print("*         {:8.2f}           *").format(z)
+            print("*                            *")
+            print("*                            *")
+            print("******************************")
 
         elif userInput == 5:
             x = raw_input("What is the first number you would like get the power of?  ")
@@ -88,17 +133,60 @@ while input == "yes":
             y = raw_input("What is the secound number you would like the previous number powered to?  ")
             y = float(y)
             z = power(x, y)
-            print("{} ^ {} = {}").format(x, y, z)
+            # print("{} ^ {} = {}").format(x, y, z)
+            print("******************************")
+            print("*                            *")
+            print("*                            *")
+            print("*                            *")
+            print("*{:8.2f}     ^ {:8.2f} =   *").format(x, y)
+            print("*                            *")
+            print("*                            *")
+            print("*   {:15.2f}          *").format(z)
+            print("*                            *")
+            print("*                            *")
+            print("******************************")
 
         elif userInput == 6:
             x = raw_input("What is number do you want to Factor?  ")
-            x = float(x)
-            print(fac(x))
+            x = int(x)
+            if x > 22:
+                print("ERROR:  Input larger than 22\n")
+                continue
+            # print(fac(x))
+            print("******************************")
+            print("*                            *")
+            print("*                            *")
+            print("*                            *")
+            print("*      Factor of  {:-4} =     *").format(x)
+            print("*                            *")
+            print("*                            *")
+            print("*    {:-22}  *").format(fac(x))
+            print("*                            *")
+            print("*                            *")
+            print("******************************")
 
         elif userInput == 7:
             x = raw_input("What is number do you want to Fibonacci?  ")
-            x = float(x)
-            print(fib(x))
+            x = int(x)
+            if x > 100:
+                print("ERROR:  Input larger than 100\n")
+                continue
+            
+            # print(fib(x))
+            print("******************************")
+            print("*                            *")
+            print("*                            *")
+            print("*                            *")
+            print("*    Fibonacci of {:-4} =     *").format(x)
+            print("*                            *")
+            print("*                            *")
+            print("*    {:-22}  *").format(fib(x))
+            print("*                            *")
+            print("*                            *")
+            print("******************************")
+        
+        else:
+            print("ERROR:  Invalid Selection")
 
         anotherCalcualtion = raw_input("Would you like to perform another calculation? (yes/no)  ")
         if anotherCalcualtion == "yes":
