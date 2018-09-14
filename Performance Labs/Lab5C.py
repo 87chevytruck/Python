1b1ec85702d68e10951c7dd9e4e3cf9b036e0a85
Ricky Smith <ricky.smith.8@outlook.com>
2018-09-14 09:39:15 -0500

    Lab5C using class inheritence added

---
+++ Performance Labs/Lab5C.py
@@ -0,0 +1,91 @@
+""" Ricky Smith, Lab5C:  Update Hero Class, 14 Sep 2018 """
+
+# create Person Class
+class Person(object):
+    def __init__(self, fName, lName, age, birthPlace):
+        self.__fName = fName
+        self.__lName = lName
+        self.__age = age
+        self.__birthPlace = birthPlace
+    
+    def setFname(self, fName):
+        self.__fName = fName
+
+    def getFname(self):
+        return self.__fName
+
+    def setLname(self, lName):
+        self.__lName = lName
+
+    def getLname(self):
+        return self.__lName
+    
+    def setAge(self, age):
+        self.__age = age
+
+    def getAge(self):
+        return self.__age
+    
+    def getBirthPlace(self):
+        return self.__birthPlace
+
+    def setBirthPlace(self, birthPlace):
+        self.__birthPlace = birthPlace
+
+
+# create Hero Class
+class Hero(Person):
+    def __init__(self, heroName, heroColor, birthPlanet):
+        # Person.__init__(self, heroName)
+        self.heroName = heroName
+        # self.realName = person.fName + " " + person.lName
+        # self.superPower = superPower
+        self.heroColor = heroColor
+        self.birthPlanet = birthPlanet
+
+    def printDetails(self):
+        print("Hero's Name:  {}").format(self.heroName)
+        print("Hero's Real Name:  {} {}").format(self.getFname(), self.getLname())
+        print("Hero's Age:  {}").format(self.getAge())
+        print("Hero's Super Power:  {}").format(self.getSuperPower())
+        print("Hero's Costume Color:  {}").format(self.heroColor)
+        print("Hero's Birth Place:  {}").format(self.getBirthPlace())
+        print("Hero's Home World:  {}").format(self.getBirthPlanet())
+
+    def getSuperPower(self):
+        return self.superPower
+
+    def setSuperPower(self, superPower):
+        self.superPower = superPower
+    
+    def getBirthPlanet(self):
+        return self.birthPlanet
+
+    def setBirthPlanet(self, birthPlanet):
+        self.birthPlanet = birthPlanet
+
+# set hero 1 and 2's information using the class Invisible_Hero
+hero1 = Hero("The Invisible Hulk", "N/A (he's sort of invisible)", 
+                       "Blackhole X-43")
+hero2 = Hero("The Incredible Lady", "N/A (she's sort of invisible)", 
+             "Lunar S-7")
+
+hero1.setAge(143)
+hero1.setFname("Jack")
+hero1.setLname("Smith")
+hero1.setSuperPower("Super Strength and Inisibility")
+hero1.setBirthPlace("Secret Location")
+
+hero2.setAge(718)
+hero2.setFname("Aria")
+hero2.setLname("Luna")
+hero2.setSuperPower("God Like Powers (Immortal, Telekinetic, Telepathic, "
+                    "Invisibility, Super Strength, etc.)")
+hero2.setBirthPlace("UNKNOWN")
+
+# display hero 1 and 2's info
+print("= = = = = = = Hero 1 = = = = = = =")
+hero1.printDetails()
+print("\n= = = = = = = Hero 2 = = = = = = =")
+hero2.printDetails()
+
