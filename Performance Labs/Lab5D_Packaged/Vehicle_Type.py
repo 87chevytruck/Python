1ef505fae3a716db0cd3ab6b1408a040a16de629
Ricky Smith <ricky.smith.8@outlook.com>
2018-09-17 09:46:43 -0500

    added Engine, Abilities, and Options classes

--- Performance Labs/Lab5D_Packaged/Vehicle_Type.py
+++ Performance Labs/Lab5D_Packaged/Vehicle_Type.py
@@ -3,7 +3,12 @@
 # create Vehicle Type class
 class Vehicle_Type:
     def __init__(self, vType):
-        def getVehicleType():
+        self.vType = vType
+
+        def setVehicleType(self, vType):
+            self.vType = vType
+
+        def getVehicleType(self):
             if vType == "truck":
                 return "truck"
             elif vType == "car":
@@ -43,3 +48,77 @@ class Engine:
     def getHP(self):
         return self.HP
 
+    def setTQ(self, TQ):
+        if TQ > 2500:
+            return "ERROR:  Too much TQ."
+        elif TQ < 50:
+            return "ERROR:  Too little TQ."
+        else:
+            self.TQ = TQ
+
+    def getTQ(self):
+        return self.TQ
+
+class Vehicle_Abilities:
+    def __init__(self, vTowing, vWeight, vDoors):
+        self.vTowing = vTowing
+        self.vWeight = vWeight
+        self.vDoors = vDoors
+
+    def setTowing(self, vTowing):
+        if vTowing < 0:
+            return "ERROR:  Tow rating must be at least 0 lbs"
+        elif vTowing > 30000:
+            return "ERROR:  Tow rating is too high"
+        else:
+            self.vTowing = vTowing
+    
+    def getTowing(self):
+        return self.vTowing
+
+    def setWeight(self, vWeight):
+        if vWeight < 1800:
+            return "ERROR:  Vehicle weight must be at least 1,800 lbs"
+        elif vWeight > 30000:
+            return "ERROR:  Vehicle weight must be under 30,000 lbs"
+        else:
+            self.vWeight = vWeight
+    
+    def getWeight(self):
+        return self.vWeight
+
+    def setDoors(self, vDoors):
+        if vDoors < 2:
+            return "ERROR:  Vehicle must have at least 2 doors"
+        elif vDoors > 5:
+            return "ERROR:  Vehicle must have no more than 5 doors"
+        else:
+            self.vDoors = vDoors
+    
+    def getDoors(self):
+        return self.vDoors
+
+class Vehicle_Options:
+    def __init__(self, powerWindows, powerLocks, nav, premAudio, backCam, leatherSeats, heatedSeats, ventedSeats, blindSpot, autoTrans, driveTrain):
+        self.powerWindows = powerWindows
+        self.powerLocks = powerLocks
+        self.nav = nav
+        self.premAudio = premAudio
+        self.backCam = backCam
+        self.leatherSeats = leatherSeats
+        self.heatedSeats = heatedSeats
+        self.ventedSeats = ventedSeats
+        self.blindSpot = blindSpot
+        self.autoTrans = autoTrans
+        self.driveTrain = driveTrain
+
+    def setPowerWindows(self, powerWindows):
+        if powerWindows == "Yes" or powerWindows == "yes" or powerWindows == "Y" or powerWindows == "y":
+            return "Yes"
+        elif powerWindows == "No" or powerWindows == "no" or powerWindows == "N" or powerWindows == "n":
+            return "No"
+        else:
+            return "ERROR:  You must enter Yes or No"
+    
+    def test(self):
+        print("{}").format(self.vWeight)
