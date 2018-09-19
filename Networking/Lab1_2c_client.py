0e70a39caa39306f80f42b498a5ab05371322f51
Ricky Smith <ricky.smith.8@outlook.com>
2018-09-19 13:29:13 -0500

    added Lab1-2c server and client, added wireshark snippets

---
+++ Networking/Lab1_2c_client.py
@@ -0,0 +1,14 @@
+""" Ricky Smith, Networking:  Lab 1-2A Client, 19 Sept 2018 """
+
+from socket import *
+
+s = socket(AF_INET, SOCK_DGRAM)
+# s.connect(("192.168.31.131", 12345))
+
+s.sendto("Hello Server, my name is udpClient.",("192.168.31.131", 12345))
+
+data, addr = s.recvfrom(1024)
+
+
+print(data) 
+
